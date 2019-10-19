8d7d01ada8b2404a4085c9c7291709f2f94ca667
jonathansnyder <46977972+jonathansnyder@users.noreply.github.com>
2019-10-18 20:07:19 -0600

    Added in the led digital write logic functions

--- Running.c
+++ Running.c
@@ -12,18 +12,45 @@ String request = client.readStringUntil('\r');
 client.flush();
 
 if(request.indexof("/Green")){
-
+    greenled();
 }
 
 if(request.indexof("/Yellow")){
-
-
+    yellowled();
 }
 
 if(request.indexof("/Red")){
-
+    redled();
 }
 
 if(request.indexof("/Auto")){
+
+}
+
+void greenled(){
+    digitalWrite(green, HIGH);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+}
+
+void redled(){
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+}
+
+void yellowled(){
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(red, LOW);
+}
+
+void offled(){
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+}
+
+void autocycle(){
 
 }
